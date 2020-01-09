6e4c4e431af8bf20e00c3de36c70389d372c51a9
manojgangwar01 <58832856+manojgangwar01@users.noreply.github.com>
2020-01-10 10:39:42 +1100

    Update Widget.js

--- widgets/CADExport/Widget.js
+++ widgets/CADExport/Widget.js
@@ -1260,11 +1260,11 @@ function(
                    val = "on";
                }
         
-        var target_url = "http://GDFM02VP:8080/fmejobsubmitter/test/WebApp_CAD_Export_v01_.fmw?email=" + this.emailField.value + "&ProjectNumber=" +this.projectField.value+ "&DrawingNumber=" +this.drawingField.value+ "&ProjectDesc=" +this.descriptionField.value+ "&fullName=" +this.nameField.value+ "&Comments=" +this.commentsField.value+"&Shape__Area=" +this.AreaValidation(geom, curr_graph) + "&UGDetails=" +val + "&_coordinates=" +geom.rings+ "&opt_showresult=true&opt_servicemode=sync&token=c6ad36b0484ecab087a078b03023cb6f7adf90dd";
+        var target_url = "http://GDFM02VP:8080/fmejobsubmitter/test/WebApp_CAD_Export_v01_test.fmw?email=" + this.emailField.value + "&ProjectNumber=" +this.projectField.value+ "&DrawingNumber=" +this.drawingField.value+ "&ProjectDesc=" +this.descriptionField.value+ "&fullName=" +this.nameField.value+ "&Comments=" +this.commentsField.value+"&Shape__Area=" +this.AreaValidation(geom, curr_graph) + "&UGDetails=" +val + "&_coordinates=" +geom.rings+ "&opt_showresult=true&opt_servicemode=sync&token=c6ad36b0484ecab087a078b03023cb6f7adf90dd";
         
        
         
-       // run_ajax(target_url);
+        run_ajax(target_url);
         /* var layerRequest = esriRequest({
                    url: target_url,                   
                    content:{f: "json"}
